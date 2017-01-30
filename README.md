34f6f110ad219b263fd3dcfb59b671b65a12e6dc
ankushpatel <ankush@loktra.com>
2017-01-30 17:31:26 +0530

    readme changes

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-Following should be our aim:
-1) Smoothing route.
-2) Avoiding inaccurate points.
-3) Calculate distance.
-4) Build route with minimum possible points.
+Here we have few data points in csv format.
+These files contains x,y co-ordinates and timestamp with accuracy captured
+by gps sensor.
+
+Also provided a sample python script which will read given csv file
+and plot it using matplot.
+
+Following should be our aim:<br>
+1) Smoothing route.<br>
+2) Avoiding inaccurate points.<br>
+3) Calculate distance.<br>
+4) Build route with minimum possible points.<br>
+
+
+
--- just_plot.py
+++ just_plot.py
@@ -4,7 +4,7 @@ import csv
 
 # read csv file
 sample_array = []
-file_path = "/home/ankush/py_pack/some-quest/sample_points1.csv"
+file_path = "/home/ankush/py_pack/some-quest/sample_points2.csv"
 with open(file_path, 'rb') as csvfile:
     print "Reading File."
     datareader = csv.reader(csvfile, delimiter=',')
@@ -20,7 +20,10 @@ raw_data = sample_array[1:]
 
 # get only data
 data = []
+better_data = []
 for d in raw_data:
+    if float(d[3]) < 50:
+        better_data.append([float(d[0]),float(d[1])])
     data.append([float(d[0]),float(d[1])])
 
 print "Data Points"
@@ -31,10 +34,14 @@ print data[:5]
 x = [j for i,j in data]
 y = [i for i,j in data]
 print x[:5], y[:5]
+
+x1 = [j for i,j in better_data]
+y1 = [i for i,j in better_data]
 # create figure on matplot
 fig = plt.figure()
 ax = fig.gca()
 ax.plot(x,y)
+ax.plot(x1,y1)
 ax.axis('scaled')
 plt.show()
 
