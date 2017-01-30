851f163dc043ac11f8108fb3b0f75a90bbe8f766
ankushpatel <ankush@loktra.com>
2017-01-30 15:56:33 +0530

    plot and data

--- just_plot.py
+++ just_plot.py
@@ -4,27 +4,33 @@ import csv
 
 # read csv file
 sample_array = []
-file_path = "/path/to/data.csv"
+file_path = "/home/ankush/py_pack/some-quest/sample_points2.csv"
 with open(file_path, 'rb') as csvfile:
+    print "Reading File."
     datareader = csv.reader(csvfile, delimiter=',')
     for row in datareader:
         sample_array.append(row)
+    print len(sample_array)
+
+print "Sample Data"
+print sample_array[:5]
 
 #remove header
 raw_data = sample_array[1:]
 
 # get only data
 data = []
-better_data = []
 for d in raw_data:
     data.append([float(d[0]),float(d[1])])
 
+print "Data Points"
+print data[:5]
 # ---------logic-----------#
 
 # extract x,y for plot
-x = [j for i,j in better_data]
-y = [i for i,j in better_data]
-
+x = [j for i,j in data]
+y = [i for i,j in data]
+print x[:5], y[:5]
 # create figure on matplot
 fig = plt.figure()
 ax = fig.gca()
